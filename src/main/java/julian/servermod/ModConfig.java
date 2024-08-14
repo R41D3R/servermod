e34a42931be4fd0816d7132138391daa1684c827
Julián Schulze <juli23072@gmail.com>
2024-08-14 18:31:03 +0200

    - implemented hopper performance increase from easy hopper mod

---
+++ src/main/java/julian/servermod/ModConfig.java
@@ -0,0 +1,13 @@
+package julian.servermod;
+
+public class ModConfig {
+
+    public final static int HOPPER_TRANSFER_COOLDOWN = 80;
+    public final static int HOPPER_INPUT_COUNT = 10;
+    public final static int HOPPER_OUTPUT_COUNT = 10;
+    public final static boolean HOPPER_CLASSIFICATION = false;
+    public final static boolean HOPPER_BETTER_EXTRACT = false;
+    public final static boolean HOPPER_EXTRACT_COOLDOWN = false;
+    public final static int HOPPER_MINECART_TRANSFER_COOLDOWN = 1;
+    public final static boolean HOPPER_MINECART_BETTER_EXTRACT = false;
+}
